8a29f2b33b7186f29b15234fb7cfa86d3f2f4a8f
Elia Musiu <elia.musiu@studenti.unimi.it>
2020-08-27 12:25:31 +0200

    Invio Json (registrazione clienti) con php

--- pages/submitForm.php
+++ pages/submitForm.php
@@ -4,21 +4,46 @@
           
         function get_data() { 
             $datae = array(); 
-            $datae[] = array( 
-                'Nome' => $_POST['nome'], 
-                'Cognome' => $_POST['cognome'], 
-                'Indirizzo' => $_POST['indirizzo'], 
-            ); 
-            return json_encode($datae); 
+            
+            $array = json_decode(file_get_contents("../../FastFood/Utenti.json"), true);
+            if ($array != null) {
+                $datae = array( 
+                    'nome' => $_POST['nome'], 
+                    'cognome' => $_POST['cognome'], 
+                    'indirizzo' => $_POST['indirizzo'],
+                    'comune' => $_POST['comune'],
+                    'provincia' => $_POST['provincia'],
+                    'CAP' => $_POST['CAP'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password'],
+                    'prefCibo' => $_POST['preferenzeCibo'],
+                    'prefPagamento' => $_POST['preferenzePagamento']
+                );
+                array_push($array, $datae);
+                return json_encode($array);
+            } else {
+                $datae[] = array( 
+                    'nome' => $_POST['nome'], 
+                    'cognome' => $_POST['cognome'], 
+                    'indirizzo' => $_POST['indirizzo'],
+                    'comune' => $_POST['comune'],
+                    'provincia' => $_POST['provincia'],
+                    'CAP' => $_POST['CAP'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password'],
+                    'prefCibo' => $_POST['preferenzeCibo'],
+                    'prefPagamento' => $_POST['preferenzePagamento']
+                );
+                return json_encode($datae); 
+            }
         } 
-          
        
         if(file_put_contents( 
-            "../../FastFood/Utenti.json", get_data(), FILE_APPEND)) { 
+            "../../FastFood/Utenti.json", get_data())) { 
                 echo'File created';
             } 
         else { 
             echo 'There is some error'; 
         } 
     } 
-?> 
+?>
--- script/sendJson.js
+++ script/sendJson.js
@@ -1,11 +1,11 @@
 function submitData() {
-    console.log("1")
     var name = document.getElementById("nameInputText").value
     var surname = document.getElementById("surnameInputText").value
+    var address = document.getElementById("addressInputText").value
     var xhr = new window.XMLHttpRequest()
     xhr.open('POST', "http://localhost/fastfood/Utenti.json", true)
     xhr.setRequestHeader('Content-Type', 'application/json')
-    var data = JSON.stringify({ "Nome": name.value, "Cognome": surname.value });
+    var data = JSON.stringify({ "nome": name, "cognome": surname, "indirizzo": address });
+    console.log(data)
     xhr.send(data)
-    console.log("qui")
 }
