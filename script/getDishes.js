38f6ca5517ed0b95ff5638b00ffb6100ef2ebd3b
andre <andrea.liotta@studenti.unimi.it>
2020-09-03 11:12:58 +0200

    03/09 prima modifica

--- script/getDishes.js
+++ script/getDishes.js
@@ -1,4 +1,3 @@
-console.log("Qui")
 var request = new XMLHttpRequest()
 request.open("GET", "http://localhost/FastFood/Panini.json", true)
 request.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
@@ -8,30 +7,36 @@ request.onreadystatechange = function(e) {
     if (request.readyState == 4 && this.status == 200) {
         sessionStorage.setItem("users", this.response)
         var dish = JSON.parse(this.response).comuni
+
         for (var i = 0; i < dish.length; i++) {
             document.getElementById("commonDishes").innerHTML += '<li href="#" class="list-group-item">' +
                 '<div class="form-check form-check-inline mt-0">' +
-                '<input class="form-check-input" type="checkbox" name="dishCheckbox[]" value="' + dish[i].nome + '">' +
+                '<input class="form-check-input" type="checkbox" checked name="dishCheckbox[]" value="' + dish[i].nome + '">' +
                 '<label class="form-check-label mx-2">' + dish[i].nome + '</label>' +
                 '</div></li>'
         }
-        var customDish = JSON.parse(this.response).personalizzati
-        for (var i = 0; i < customDish.length; i++) {
-            document.getElementById("customizedDishes").innerHTML += '<li href="#" class="list-group-item">' +
-                '<div class="form-check form-check-inline mt-0">' +
-                '<input class="form-check-input" type="checkbox" name="dishCheckbox[]" value="' + customDish[i].nome + '">' +
-                '<label class="form-check-label mx-2">' + customDish[i].nome + '</label></div>' +
-                '<button class="btn btn-danger mx-1 float-right" type="button">' +
-                '   <i class="fas fa-trash-alt fa-fw"></i> Elimina ' +
-                '</button>' +
-                '<button class="btn btn-secondary mx-1 float-right" type="button" data-toggle="modal" data-target="#editDishModal">' +
-                '   <i class="fas fa-edit fa-fw"></i> Modifica ' +
-                '   </button>' +
-                '</li>'
-            document.getElementById("modalTitle").innerText = customDish[i].nome
-            document.getElementById("nameEdit").value = customDish[i].nome
-            document.getElementById("typeEdit").value = customDish[i].tipologia
-            document.getElementById("priceEdit").value = customDish[i].prezzo
-        }
+
+        var restaurants = JSON.parse(this.response).paniniRistoranti
+        for (var j = 0; j < restaurants.length; j++){
+            if (restaurants[j].email == JSON.parse(sessionStorage.getItem('actualUser')).email) {
+                var customDish = restaurants[j].paniniPersonalizzati
+                for (var i = 0; i < customDish.length; i++) {
+                    document.getElementById("customizedDishes").innerHTML += '<li href="#" class="list-group-item">' +
+                        '<div class="form-check form-check-inline mt-0">' +
+                        '<label class="form-check-label mx-2">' + customDish[i].nome + '</label></div>' +
+                        '<button class="btn btn-danger mx-1 float-right" type="button">' +
+                        '   <i class="fas fa-trash-alt fa-fw"></i> Elimina ' +
+                        '</button>' +
+                        '<button class="btn btn-secondary mx-1 float-right" type="button" data-toggle="modal" data-target="#editDishModal">' +
+                        '   <i class="fas fa-edit fa-fw"></i> Modifica ' +
+                        '   </button>' +
+                        '</li>'
+                    document.getElementById("modalTitle").innerText = customDish[i].nome
+                    document.getElementById("nameEdit").value = customDish[i].nome
+                    document.getElementById("typeEdit").value = customDish[i].tipologia
+                    document.getElementById("priceEdit").value = customDish[i].prezzo
+                }
+            }
+        }      
     }
 }
